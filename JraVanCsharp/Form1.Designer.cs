d65c03ee7be6163d50470b0c3b8d29c686656698
specially198 <more.specially198@gmail.com>
2023-08-06 15:10:15 +0900

    Lesson2 JV-Data をダウンロードする

--- JraVanCsharp/Form1.Designer.cs
+++ JraVanCsharp/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace JraVanCsharp
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.mnuConfig = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuConfJV = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGetJVData = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axJVLink1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -55,7 +56,7 @@ namespace JraVanCsharp
             this.mnuConfig});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 42);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 40);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,7 +65,7 @@ namespace JraVanCsharp
             this.mnuConfig.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuConfJV});
             this.mnuConfig.Name = "mnuConfig";
-            this.mnuConfig.Size = new System.Drawing.Size(112, 38);
+            this.mnuConfig.Size = new System.Drawing.Size(112, 36);
             this.mnuConfig.Text = "設定(&C)";
             // 
             // mnuConfJV
@@ -74,16 +75,28 @@ namespace JraVanCsharp
             this.mnuConfJV.Text = "JV-Link の設定(&J)...";
             this.mnuConfJV.Click += new System.EventHandler(this.mnuConfJV_Click);
             // 
+            // btnGetJVData
+            // 
+            this.btnGetJVData.Location = new System.Drawing.Point(21, 56);
+            this.btnGetJVData.Name = "btnGetJVData";
+            this.btnGetJVData.Size = new System.Drawing.Size(157, 79);
+            this.btnGetJVData.TabIndex = 2;
+            this.btnGetJVData.Text = "データ取得";
+            this.btnGetJVData.UseVisualStyleBackColor = true;
+            this.btnGetJVData.Click += new System.EventHandler(this.btnGetJVData_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetJVData);
             this.Controls.Add(this.axJVLink1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axJVLink1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -98,6 +111,7 @@ namespace JraVanCsharp
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem mnuConfig;
         private System.Windows.Forms.ToolStripMenuItem mnuConfJV;
+        private System.Windows.Forms.Button btnGetJVData;
     }
 }
 
--- JraVanCsharp/Form1.cs
+++ JraVanCsharp/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,21 @@ namespace JraVanCsharp
             InitializeComponent();
         }
 
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // 引数設定
+            string sid = "Test";
+
+            // JVLink 初期化
+            int lReturnCode = axJVLink1.JVInit(sid);
+
+            // エラー判定
+            if (lReturnCode != 0)
+            {
+                MessageBox.Show("JVInit エラー コード：" + lReturnCode);
+            }
+        }
+
         private void mnuConfJV_Click(object sender, EventArgs e)
         {
             // 設定画面表示
@@ -24,6 +40,58 @@ namespace JraVanCsharp
             if (lReturnCode != 0)
             {
                 MessageBox.Show("JVSetUIPropertiesエラー コード：" + lReturnCode);
+            }
+        }
+
+        private void btnGetJVData_Click(object sender, EventArgs e)
+        {
+            int lReturnCode;
+            try
+            {
+                // 引数設定
+                // JVOpen:ファイル識別子
+                string strDataSpec = "RACE";
+                // JVOpen:データ提供日付
+                string strFromTime = "20050301000000";
+                // JVOpen:オプション
+                int lOption = 2;
+
+                // JVLink 戻り値
+                int lReadCount = 0;
+                // JVOpen: 総ダウンロードファイル数
+                int lDownloadCount = 0;
+                // JVOpen: 最新ファイルのタイムスタンプ
+                string strLastFileTimestamp;
+
+                // JVLink ダウンロード処理
+                lReturnCode = axJVLink1.JVOpen(
+                    strDataSpec, strFromTime, lOption, ref lReadCount, ref lDownloadCount, out strLastFileTimestamp);
+
+                // エラー判定
+                if (lReturnCode != 0)
+                {
+                    MessageBox.Show("JVOpen エラー：" + lReturnCode);
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "戻り値 : " + lReturnCode + "\n"
+                        + "読み込みファイル数 : " + lReadCount + "\n"
+                        + "ダウンロードファイル数 : " + lDownloadCount + "\n"
+                        + "タイムスタンプ : " + strLastFileTimestamp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return;
+            }
+
+            // JVLink 終了処理
+            lReturnCode = axJVLink1.JVClose();
+            if (lReturnCode != 0)
+            {
+                MessageBox.Show("JVClose エラー：" + lReturnCode);
             }
         }
     }
