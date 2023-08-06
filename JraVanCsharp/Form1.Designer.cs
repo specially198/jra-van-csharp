a15a872f91edbcf9b5b38d14f18add73ba3de5c5
specially198 <more.specially198@gmail.com>
2023-08-06 11:16:48 +0900

    新規プロジェクト作成

---
+++ JraVanCsharp/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace JraVanCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axJVLink1 = new AxJVDTLabLib.AxJVLink();
+            ((System.ComponentModel.ISupportInitialize)(this.axJVLink1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axJVLink1
+            // 
+            this.axJVLink1.Enabled = true;
+            this.axJVLink1.Location = new System.Drawing.Point(645, 230);
+            this.axJVLink1.Name = "axJVLink1";
+            this.axJVLink1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axJVLink1.OcxState")));
+            this.axJVLink1.Size = new System.Drawing.Size(384, 384);
+            this.axJVLink1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axJVLink1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axJVLink1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxJVDTLabLib.AxJVLink axJVLink1;
+    }
+}
+
